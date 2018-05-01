f26bfdb356ac60d7f4205ea0e1c122e1184e6414
dayan <dayansfo@yahoo.com>
2018-05-01 13:20:57 -0700

    updated version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var win = 0; 
 var Losses= 0; 
-var TotalScore = 0; 
+var totalScore = 0; 
+var previous = 0;
 
 // radom number from 1 to 120
 var target_result = Math.floor(Math.random() * 90 ) + 30;
@@ -14,9 +15,7 @@ var images = [
     'assets/images/yellow.jpg',
 ]
 
-// var resetAndStart = function () {
-//   $(".crystals").empty();
-// }
+
 
 $("#result").html('Target Result: ' + target_result);
 
@@ -27,55 +26,38 @@ $("#red").attr("dataValue", Math.floor(Math.random() * 11) + 1);
 $("#yellow").attr("dataValue", Math.floor(Math.random() * 11) + 1);
 $("#pink").attr("dataValue", Math.floor(Math.random() * 11) + 1);
 
-// Score
-
-$("#previous").html("Total Score: " + previous);
-
 // click event
 
 $(document).on('click', '.crystalsImg', function () {
     console.log(this);
 
-  var num = parseInt($(this).attr('dataValue'));
-  previous += num;
-  console.log(num);
-
-//   Score message
-// alert("New score: " + counter);
-
-// if (counter === targetNumber) {
-//   alert("You win!");
-//   resetGame();
-// }
-
-// else if (counter >= targetNumber) {
-//   alert("You lose!!");
-//   resetGame();
-// }
-
-// function resetGame(){
-//   counter = 0;
-//   targetNumber = Math.floor(Math.random() * 50);
-// }
+    var num = parseInt($(this).attr('datavalue'));
+    previous += num;
+    console.log(num);
+    
+    stillPlaying();
 
 });
 
+function stillPlaying() {
 
-    $("#previous").html("Total score: " + previous);
-    console.log(previous);
+    if (previous === target_result) {
+        alert("You win! Press 'ok' to play again.");
+        win++
+        $("#winBox").html(win);
+        reset();
 
-  if(previous > target_result){
-    lost--;
-    $("#lost").html("You lost" + lost);
-    previous = 0;
-    resetAndStart();
+    } else if (previous > target_result) {
+        alert("You lose! Press 'ok' to play again.");
+        loss++
+        $("#lossBox").html(loss);
+        reset();
+    }
 
-  }
-  else if(previous == target_result){
-    win++;
-    $("#win").html("You Win" + win);
-    previous = 0;
-    resetAndStart();
-  }
-  
-// });
+    $("#previous").text(previous);
+
+var resetAndStart = function () {
+  $(".crystals").empty();
+
+}
+}
